0f6dfbd1fdb2c6631c3249c7f715b7b35e7fea87
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-06-01 15:18:06 +0300

    added tutorial1

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ fs.writeFile("message.txt","hello",(err, data) => {
     if (err) throw err;
     console.log("the file has been saved!");
 });
+
+fs.readFile("./message.txt","utf8",(err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
